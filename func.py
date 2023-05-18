4893bce3d6bd8cd036d4124ebec401de473eac8d
Cristiano Hoshikawa <hoshikawa@uol.com.br>
2023-05-18 20:56:13 -0300

    Adjustments

--- func.py
+++ func.py
@@ -5,6 +5,7 @@ import datetime
 import jwt
 import requests
 import base64
+import oci
 
 from datetime import timedelta
 from fdk import response
@@ -14,11 +15,20 @@ def handler(ctx, data: io.BytesIO = None):
     token = "invalid"
     apiKey = "invalid"
     expiresAt = (datetime.datetime.utcnow() + timedelta(seconds=60)).replace(tzinfo=datetime.timezone.utc).astimezone().replace(microsecond=0).isoformat()
-    
+
+    config = oci.config.from_file("config")
+    object_storage = oci.object_storage.ObjectStorageClient(config)
+    namespace = object_storage.get_namespace().data
+
     try:
         auth_token = json.loads(data.getvalue())
         secretID = auth_token.get("secretID")
         clientID = auth_token.get("clientID")
+        objectID = auth_token.get("objectID")
+
+        get_obj = object_storage.get_object(namespace, "data", objectID)
+        objb64 = base64.b64encode(get_obj.data.content)
+        objstr = objb64.decode('utf-8')
 
         auth = clientID + ":" + secretID
         auth_bytes = auth.encode("ascii")
@@ -27,29 +37,29 @@ def handler(ctx, data: io.BytesIO = None):
 
         headers = {"Authorization": "Basic " + auth_base64_message, "Content-Type": "application/x-www-form-urlencoded"}
 
-        scope = "<any scope if you have>"
+        scope = "xxxxxxxxxxxxxxxxxxxxxx.apigateway.us-ashburn-1.oci.customer-oci.com/super-scope"
         grant_type = "client_credentials"
 
         body = {"scope": scope, "grant_type": grant_type}
 
-        url_post = "https://idcs-4f....................e09.identity.oraclecloud.com/oauth2/v1/token"
+        url_post = "https://idcs-xxxxxxxxxxxxxxxxxxxxxxx.identity.oraclecloud.com/oauth2/v1/token"
         post_response = requests.post(url_post, headers=headers, data=body)
 
         jwtTokenDecoded = jwt.decode(post_response.json()['access_token'], options={"verify_signature": False})
 
         return response.Response(
-            ctx, 
-            status_code=200, 
-            response_data=json.dumps({"active": True, "principal": "foo", "scope": "bar", "clientId": "1234", "expiresAt": expiresAt, "context": {"username": "wally", "token": post_response.json()['access_token'], "jwtTokenDecoded": jwtTokenDecoded}})
-         )
-    
+            ctx,
+            status_code=200,
+            response_data=json.dumps({"active": True, "principal": "foo", "scope": "bar", "clientId": "1234", "expiresAt": expiresAt, "context": {"username": "wally", "token": post_response.json()['access_token'], "jwtTokenDecoded": jwtTokenDecoded, "object": objstr}})
+        )
+
     except (Exception, ValueError) as ex:
         logging.getLogger().info('error parsing json payload: ' + str(ex))
         pass
-    
+
     return response.Response(
-       ctx, 
-        status_code=401, 
+        ctx,
+        status_code=401,
         response_data=json.dumps({"active": False, "wwwAuthenticate": "API-key"})
     )
 
